4d103e79f468294d1ec03e26d0bd3ef5e1e1ae76
Sam Pomerantz <sam@sampomerantz.me>
2023-04-12 15:01:04 -0400

    loading the data directly from python

--- graph.py
+++ graph.py
@@ -4,16 +4,15 @@ import matplotlib.pyplot as plt
 import webbrowser
 from pyvis.network import Network
 
+#https://github.com/simonlindgren/nXd3/tree/master
+
 import community
 
 database = DataBase()
 
-limit_size = True
 seen_stocks = database.get_indexed_similar_tickers()
-if limit_size:
-    seen_stocks = seen_stocks[:60]
 
-G = nx.Graph()
+G = nx.Graph(useDot=True)
 
 for stock in seen_stocks:
     G.add_node(stock)
@@ -23,7 +22,7 @@ for stock in seen_stocks:
         if index < 5:
             G.add_edge(stock, similar_stock)
 
-partition = community.best_partition(G, resolution=6)
+partition = community.best_partition(G, resolution=20)
 
 subgraphs = {}
 for node, part in partition.items():
@@ -39,30 +38,27 @@ for edge in G.edges():
 
 combined_graph = nx.Graph()
 
+#construct the MST in each subgraph
+# for subgraph in subgraphs.values():
+#     mst = nx.minimum_spanning_tree(subgraph)
+#     combined_graph = nx.compose(combined_graph, mst)
+
 for subgraph in subgraphs.values():
     combined_graph = nx.compose(combined_graph, subgraph)
 
 for node in combined_graph.nodes():
     combined_graph.nodes[node]['color'] = partition[node]
-    combined_graph.nodes[node]['size'] = combined_graph.degree(node) * 1.5
+    combined_graph.nodes[node]['size'] = combined_graph.degree(node) * 2.5
 
-#nx.draw(combined_graph, with_labels=True)
-#plt.show()
-
-net = Network(bgcolor="#222222", font_color="white", height="100%", width="100%")
+net = Network(bgcolor="#222222", font_color="white", height="100rem", width="100%")
 net.from_nx(combined_graph)
 net.toggle_physics(True)
 net.toggle_drag_nodes(True)
 net.toggle_hide_edges_on_drag(True)
 
-net.save_graph("index.html")
+#net.save_graph("index2.html")
+nodes, edges, heading, height, width, options = net.get_network_data()
 
-webbrowser.open("file:///Users/sampomerantz/Documents/GitHub/StockClustering/index.html")
-
-#visualize using Netwulf
-
-# import netwulf as nw
-
-# style_nw, cf = nw.visualize(combined_graph)
-
-# nw.save("graph.json", style_nw, cf, combined_graph)
+tmp_dict = {"nodes": nodes, "edges": edges}
+with open("graph.json", "w") as f:
+    json.dump(tmp_dict, f)
