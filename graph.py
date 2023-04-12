588a65b4502118c4ed3d22a2768c68576387447d
Sam Pomerantz <sam@sampomerantz.me>
2023-04-12 16:07:27 -0400

    limit mobile more

--- graph.py
+++ graph.py
@@ -64,5 +64,5 @@ if __name__ == "__main__":
     seen_stocks = database.get_indexed_similar_tickers()
     generate_graph(seen_stocks, "graph.json")
 
-    seen_stocks = seen_stocks[:100]
+    seen_stocks = seen_stocks[:30]
     generate_graph(seen_stocks, "graph_mobile.json")
